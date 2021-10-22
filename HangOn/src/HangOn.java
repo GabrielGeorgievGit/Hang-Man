9a0c4d55b26538984baa4384a8cc0ce3841cf94f
Gabriel Georgiev <44534520+GabrielGeorgievGit@users.noreply.github.com>
2021-10-22 16:50:16 +0300

    Update HangOn.java

--- HangOn/src/HangOn.java
+++ HangOn/src/HangOn.java
@@ -9,8 +9,7 @@ public class HangOn
 			arrayFullWord[] = WordToFind.toCharArray();
 	char[] Letters = new char[arrayNum];
 	static LinkedList<Character>WasEqualStorager = new LinkedList<Character>();
-	//Make a container for all equals letters (Not LinkedList), have to sort 'em
-	//Maybe sort is not an option :(
+	
 	 LinkedList<Character>SummonedLetters = new LinkedList<Character>();
 	public HangOn()
 	{
@@ -48,8 +47,6 @@ public class HangOn
 	void ShowLetters(LinkedList<Integer>IndexesToAdd, char letterToAdd)
 	{
 		IndexesToAdd.sort(null);
-		//System.out.println("In ShowLetters(LinkedList, char)");
-		//System.out.println("Indexes To Add: " + IndexesToAdd);
 		for(int i = 0; i < IndexesToAdd.size(); i++)
 			Letters[IndexesToAdd.get(i)] = letterToAdd;
 	}
@@ -70,11 +67,11 @@ public class HangOn
 	}
 	void OutputTheWord()
 	{
-		SummonedLetters.sort(null);//maybe remove soon?
+		SummonedLetters.sort(null);
 		System.out.println("\nThe word is:");
 		System.out.println(String.valueOf(Letters).replaceAll(".", "$0  "));
 		System.out.println("Used letters: " + SummonedLetters);
-		//System.out.println("WasEqualStorager: " + WasEqualStorager);//one day may be used... maybe
+		
 		System.out.println("Number of wrong letters: " + wrongLetterNum + "\n(Max: 8)\n");
 	}
 	boolean isEnded()
@@ -107,56 +104,46 @@ public class HangOn
 	}
 	public static void main(String[] args)throws Exception
 	{
-		/*
-		System.out.println("Working Directory = " +
-	            System.getProperty("user.dir").replaceAll( "\\'" , "/"));
-		String k = System.getProperty("user.dir");
-		String dir =k.replace( "\\" , "/");
-		System.out.println(dir);
-		*/
-		
 		Reader.Readers();
-		//uppressWarnings("resource")
-		Scanner tryContains = new Scanner(System.in);//Better tryContains than uselessShit2000
+		
+		Scanner tryContains = new Scanner(System.in);
 		HangOn word = new HangOn();
 		do
 		{
 			word.OutputTheWord();
 			System.out.print("Try letter:" );
 			word.TryLetter(tryContains.next().toCharArray());
-			//System.out.print("KYS");//Best code ever
+			
 			if(word.wrongLetterNum >= 8)
 			{
-				//word.OutputTheWord();
 				for (int i = 0; i < 50; ++i) 
-					System.out.println();//BEST CLEAR CONSOLE EVERRRR
+					System.out.println();
 				System.out.println("\n\n\nYou used more than 8 different letters\n"
 						+ "You Lost :(\nBetter KYS\nThe word was:\n" + 
-						String.valueOf(word.WordToFind).replaceAll(".", "$0  "));//Cool, huh?
+						String.valueOf(word.WordToFind).replaceAll(".", "$0  "));
 				//return;
 			}
 			if(word.isEnded() && word.wrongLetterNum < 8)
 			{
 				for (int i = 0; i < 50; ++i) 
-					System.out.println();//BEST CLEAR CONSOLE EVERRRR
+					System.out.println();
 				word.OutputTheWord();
 				System.out.println("You Win! || GG\nYo still need to KYS");
 			}
-			if(word.isEnded());//word.OutputTheWord();
+			if(word.isEnded());
 			else 
 				for (int i = 0; i < 50; ++i) 
-					System.out.println();//BEST CLEAR CONSOLE EVERRRR
+					System.out.println();
 		}
 		while(!word.isEnded());
 		
-		//word.OutputTheWord();
 		System.out.println("Press Enter to exit");
 		tryContains.nextLine();
 		tryContains.nextLine();
 		
 		System.out.println("END");
 		tryContains.close();
-		//Thread.sleep(9000L);
+		
 		System.exit(0);
 	}
 }
